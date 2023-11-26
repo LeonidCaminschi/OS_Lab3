397db8675550d24656891dadd662542cff99ccbe
ninico11 <112957880+ninico11@users.noreply.github.com>
2023-11-26 19:06:34 +0200

    Lab 3 Task 1

--- lab3.1.asm
+++ lab3.1.asm
@@ -1,12 +1,22 @@
 org 7c00h
 
-mov ch, 35
-mov cl, 1
-mov dl, 0
+mov ch, 35              ;631/18 = nr of track
+mov cl, 1               ;631 mod 18 = nr os sector
+mov dl, 0   
 mov dh, 0
 mov ax, 0               ; Clear AX
-mov es, ax               ; Set ES to 0 (segment of Memory_Buffer)
-mov bx, Name_Group    ; Move Memory_Buffer address to BX
+mov es, ax               ; Set ES to 0 (segment of Name_Group)
+mov bx, Name_Group    ; Move Name_Group address to BX
+mov ax, 0301h            ; Set AH to 03 (write sectors) and AL to 01 (sector count)
+int 13h
+
+mov ch, 37              ;660/18 = nr of track
+mov cl, 12               ;660 mod 18 = nr os sector
+mov dl, 0   
+mov dh, 0
+mov ax, 0               ; Clear AX
+mov es, ax               ; Set ES to 0 (segment of Name_Group)
+mov bx, Name_Group    ; Move Name_Group address to BX
 mov ax, 0301h            ; Set AH to 03 (write sectors) and AL to 01 (sector count)
 int 13h
 
